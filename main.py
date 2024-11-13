0df2909d0463c369a19db5a0b7ef14e863b08e35
Wesley0071 <thiagow737@gmail.com>
2024-11-12 21:22:09 -0300

    colocando comandos na sistente virtual.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import pyaudio
 import pyttsx3
 import json
+import core
 
 #Síntese de fala
 engine = pyttsx3.init()
@@ -19,15 +20,18 @@ if not os.path.exists("model"):
     print("Porfavor instale o model de https://alphacephei.com/vosk/models and unpack as 'model' in the current folder.")
     exit(1)
 
+#Reconhecimento de fala
+
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, frames_per_buffer=4096, input=True)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, frames_per_buffer=2048, input=True)
 stream.start_stream()
 
+#Loop do reconhecimento de fala
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -39,4 +43,9 @@ while True:
             print(text)
             speak(text)
 
+        if text == 'que horas são' or text == 'me diga as horas':
+            speak(core.SystemInfo.get_time())
+
+        if text == 'maria':
+            speak('pois não sônia?')
 
